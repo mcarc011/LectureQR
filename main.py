da3be87d2329f559585a0565c1e1ebb6e3cc384d
mcarc011 <mario.carcamo05@gmail.com>
2025-01-27 19:45:11 -0500

    savess

--- main.py
+++ main.py
@@ -36,6 +36,7 @@ def upload_to_dropbox(file_path, dropbox_path):
     try:
         dbx = dropbox.Dropbox(DROPBOX_ACCESS_TOKEN)
         for file_path in os.listdir():
+            st.write(file_path)
             dropbox_path = f"/{file_path}"
             with open(file_path, "rb") as f:
                 dbx.files_upload(f.read(), dropbox_path, mode=dropbox.files.WriteMode("overwrite"))
